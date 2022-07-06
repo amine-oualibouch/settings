bb3431a8675843ade3bca0cfe3189d6c41606533
Parabeac-Nest <admin@parabeac.com>
2022-07-06 23:43:41 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/theme/settings_colors.g.dart
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class SettingsColors {
+
+static const Color systemBgDarkBasePrimary = Color(0xff000000);
+
+static const Color labelLightPrimary = Color(0xff000000);
+
+static const Color labelDarkPrimary = Color(0xffffffff);
+
+static const Color labelDarkTertiary = Color(0xffebebf5);
+
+static const Color colorLightGreen = Color(0xff34c759);
+
+static const Color colorDarkYellow = Color(0xffffd60a);
+/// class var systemGreen: UIColor
+static const Color systemGreenLight = Color(0xff34c759);
+}
---
+++ lib/widgets/page_1/status_bar.g.dart
@@ -0,0 +1,48 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class StatusBar extends StatefulWidget{
+  final BoxConstraints constraints;
+  
+  const StatusBar(this.constraints, {Key? key, }) : super(key: key);
+  @override
+  _StatusBar createState() => _StatusBar();
+}
+
+class _StatusBar extends State<StatusBar>{
+  
+  _StatusBar();
+
+  @override
+  Widget build(BuildContext context){
+    
+    return Container(decoration: BoxDecoration(), child:Stack(
+children: [Positioned(left: 0,width: widget.constraints.maxWidth *1.0,top: 0,height: widget.constraints.maxHeight *1.0,child: Stack(
+children: [Positioned(left: 0,right: 0,top: 0,height: 30.0,child: LayoutBuilder( 
+  builder: (context, constraints) {
+    return Container(/** This Symbol was not found **/);
+}
+),),Positioned(left: widget.constraints.maxWidth *0.82,width: widget.constraints.maxWidth *0.014,top: widget.constraints.maxHeight *0.166,height: widget.constraints.maxHeight *0.124,child: ,),Positioned(right: 14.0,width: 69.0,top: 16.0,height: 14.0,child: Container(decoration: BoxDecoration(),child: Row(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.center,mainAxisSize: MainAxisSize.min,
+children: [Container(height: widget.constraints.maxHeight * 0.2900058799028461,width: widget.constraints.maxWidth * 0.04672897196261682,child: Container(decoration: BoxDecoration(), child:Stack(
+children: [Positioned(left: 11.0,width: 3.0,top: 4.0,height: 8.0,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 8.0,width: 3.0, fit: BoxFit.none,),),Positioned(left: 6.5,width: 3.0,top: 6.0,height: 6.0,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 6.0,width: 3.0, fit: BoxFit.none,),),Positioned(left: 2.0,width: 3.0,top: 7.5,height: 4.5,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 4.5,width: 3.0, fit: BoxFit.none,),),Positioned(left: 15.5,width: 3.0,top: 2.0,height: 10.0,child: SvgPicture.asset('assets/images/emptybar.svg', package: 'settings',height: 10.0,width: 3.0, fit: BoxFit.none,),),Positioned(left: 11.0,width: 3.0,top: 4.0,height: 8.0,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 8.0,width: 3.0, fit: BoxFit.none,),),Positioned(left: 6.5,width: 3.0,top: 6.0,height: 6.0,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 6.0,width: 3.0, fit: BoxFit.none,),),Positioned(left: 2.0,width: 3.0,top: 7.5,height: 4.5,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 4.5,width: 3.0, fit: BoxFit.none,),),]))),SizedBox(width: 4,),Container(height: widget.constraints.maxHeight * 0.2900058799028461,width: widget.constraints.maxWidth * 0.037383177570093455,child: Container(decoration: BoxDecoration(), child:Stack(
+children: [Positioned(left: 5.938,width: 4.372,top: 8.94,height: 3.06,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 3.060413360595703,width: 4.371856689453125, fit: BoxFit.none,),),Positioned(left: 3.465,width: 9.32,top: 5.47,height: 3.314,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 3.3142528533935547,width: 9.319793701171875, fit: BoxFit.none,),),Positioned(left: 1.0,width: 14.25,top: 2.0,height: 4.323,child: SvgPicture.asset('assets/images/path.svg', package: 'settings',height: 4.3225884437561035,width: 14.25, fit: BoxFit.none,),),]))),SizedBox(width: 4,),Container(height: widget.constraints.maxHeight * 0.2900058799028461,width: widget.constraints.maxWidth * 0.05841121495327103,child: Container(decoration: BoxDecoration(), child:Stack(
+children: [Positioned(left: 24.0,width: 1.0,top: 5.0,height: 4.0,child: SvgPicture.asset('assets/images/rectangle23.svg', package: 'settings',height: 4.0,width: 1.0, fit: BoxFit.none,),),Positioned(left: 0,width: 23.0,top: 1.0,height: 12.0,child: SvgPicture.asset('assets/images/rectangle21stroke.svg', package: 'settings',height: 12.0,width: 23.0, fit: BoxFit.none,),),Positioned(left: 2.0,width: 19.0,top: 3.0,height: 8.0,child: Container(height: 8.0,width: 19.0,decoration: BoxDecoration(color: Colors.white,
+borderRadius: BorderRadius.all(Radius.circular(1)),),),),]))),])),),Positioned(left: widget.constraints.maxWidth *0.049,width: widget.constraints.maxWidth *0.126,top: widget.constraints.maxHeight *0.249,height: widget.constraints.maxHeight *0.435,child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),), child:SvgPicture.asset('assets/images/941.svg', package: 'settings',height: 11.0888671875,width: 28.42616844177246, fit: BoxFit.scaleDown,)),),]),),]));
+  }
+
+      @override
+    void dispose() {
+      
+      super.dispose();
+    }
+    
+}
+
